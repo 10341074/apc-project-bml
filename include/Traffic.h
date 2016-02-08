0ab0d47db19d5ca02761b1b6ea824a5b056e1007
Giacomo Milan <milan.giacomo@libero.it>
2016-02-08 17:33:56 +0100

    copy of matrix & Makefile

---
+++ include/Traffic.h
@@ -0,0 +1,12 @@
+#ifndef TRAFFIC_H
+#define TRAFFIC_H
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+class Traffic{
+	
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <stdexcept>
+
+int main(int argc, char ** argv){
+	std::cout<<"hello"<<std::endl;
+	return 0;
+}
