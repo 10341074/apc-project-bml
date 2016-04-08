c81638b555af816bcaaaee5d6b8437e20a00c2ee
Giacomo Milan <milan.giacomo@libero.it>
2016-04-08 16:03:57 +0200

    Rebase

---
+++ src/rebase/RowsVector.cpp
@@ -0,0 +1,82 @@
+#include "RowsVector.h"
+// RowsVector::size_type DynRow::size() const {
+//	if(row != nullptr)
+//		throw std::logic_error("DynRow::size() with row = nullptr");
+//	return row->size();
+// }
+// bool DynRow::empty() const {
+//	if(row != nullptr)
+//		throw std::logic_error("DynRow::empty() with row = nullptr");
+//	return row->empty();
+// }
+// 
+// Row::iterator DynRow::begin(){
+//	if(row != nullptr)
+//		throw std::logic_error("DynRow::size() with row = nullptr");
+//	return row->begin();
+// }
+// Row::iterator DynRow::begin() const {
+//	if(row != nullptr)
+//		throw std::logic_error("DynRow::size() with row = nullptr");
+//	return row->begin();
+// }
+// //	Row::const_iterator cbegin() const;
+// Row::iterator DynRow::end() {
+//	if(row != nullptr)
+//		throw std::logic_error("DynRow::size() with row = nullptr");
+//	return row->end();
+// }
+// Row::iterator DynRow::end() const {
+//	if(row != nullptr)
+//		throw std::logic_error("DynRow::size() with row = nullptr");
+//	return row->end();
+// }
+// //	Row::const_iterator cend() const;
+
+
+void RowsVector::push_back(const DynRow & dr) {
+<<<<<<< HEAD
+//	drVec.push_back(dr);
+	return;
+}
+void RowsVector::push_back(DynRow && dr) {
+//	drVec.push_back(dr);
+	return;
+}
+void RowsVector::push_back(Row * r){
+	drVec.push_back(r);
+	cVec.push_back(r->begin());
+	cIndex = 0;
+return;
+}
+std::ostream & operator<<(std::ostream & os, const RowsVector & rowsVec) {
+	for(std::vector<DynRow>::const_iterator it = rowsVec.drVec.begin(); it != rowsVec.drVec.end(); ++it) {
+		os << * it->get_row_pointer();
+	}
+	return os;
+}
+void RowsVector::column_start() {
+	if(drVec.size() != cVec.size())
+		throw std::logic_error("RowsVector::column_start() cVec and drVec different sizes");
+	DrVec::iterator dr_it = drVec.begin();
+	for(ColumnVec::iterator it = cVec.begin(); it != cVec.end(); ++it){
+		* it = dr_it->begin();
+		++ dr_it;
+	}
+	return;
+}
+void RowsVector::column_shift() {
+	for(ColumnVec::iterator it = cVec.begin(); it != cVec.end(); ++it){
+		++ (* it);
+	}
+=======
+	std::cout << "RowsVector push_back &" << std::endl;
+	drVec.push_back(dr);
+	return;
+}
+void RowsVector::push_back(DynRow && dr) {
+	std::cout << "RowsVector push_back &&" << std::endl;
+	drVec.push_back(DynRow(dr));
+>>>>>>> Some tests on move-copy with push_back
+	return;
+}
---
+++ src/rebase/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <memory>
+#include <fstream>
+#include <string>
+
+
+#include "Template.h"
+#include "Color.h"
+#include "Row.h"
+#include "DynRow.h"
+#include "RowsVector.h"
+#include "Col.h"
+
+#include "Traffic.h"
+#include "Tokenize.h"
+
+DynRow f(){
+	DynRow dr;
+	return dr;
+}
+
+int main(int argc, char ** argv){
+	std::string if_string(argv[1]);
+	std::ifstream if_stream(if_string);
+	
+	std::string first_line;
+	std::getline(if_stream, first_line);
+	std::vector<std::size_t> v;
+	tokenize_first_line(first_line,v);
+	
+	std::cout << v ;
+	
+//	std::unique_ptr< std::vector<std::vector<Color>> > p(new std::vector<std::vector<Color>>);
+	RowsVector * p = new RowsVector;
+	Traffic traffic(p);
+	if_stream >> traffic;
+	if_stream.close();
+	
+	traffic.print();
+	
+	std::ofstream of_stream("output.csv");
+	of_stream << traffic;
+	of_stream.close();
+	
+	Row r;
+	std::cout << &r <<std::endl;
+	
+	Row * pp = nullptr;
+	delete pp;
+	
+// <<<<<<< HEAD
+// =======
+	Row row(10,Blue);
+	std::cout << row;
+	DynRow dr;
+	RowsVector rvec;
+	rvec.push_back(dr);
+	rvec.push_back(f());
+	
+// >>>>>>> Some tests on move-copy with push_back
+	return 0;
+}
