2e8a637f2fb7f362a90ce4847c4f6806beecc341
Giacomo Milan <milan.giacomo@libero.it>
2016-04-25 20:32:05 +0200

    Added clock

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@ int main(int argc, char ** argv){
 	
 	std::cout << times ;
 	
+	clock_t time = clock();
 //	std::unique_ptr< std::vector<std::vector<Color>> > p(new std::vector<std::vector<Color>>);
 //	VecVector * p = new VecVector(ByRows);
 	Traffic tr_rows(ByRows);
@@ -65,6 +66,7 @@ int main(int argc, char ** argv){
 		of << trs ;
 		of.close();
 	}
+		time=clock()-time;	std::cout<<"time =\n"<<((float)time)/CLOCKS_PER_SEC<<std::endl;	time=clock();
 
 //*/
 /*
